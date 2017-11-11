479c46e49829acd87c47b2ccc1b8c5a79cbc3d00
pbridd <pbridd@users.noreply.github.com>
2017-11-10 23:20:30 -0800

    Refactored a bunch of things

--- decompressor/decompressor_top.sv
+++ decompressor/decompressor_top.sv
@@ -12,19 +12,24 @@ module decompressor_top(
 		control_word_in,	// The control word that corresponds to data-in
 		in_data_valid,		// when this is 1'b1, the decompressor will use the inputs
 		history_full,		// FYI: the history buffer is full. Output might not be valid.
-		out_byte,			// the output byte. One byte at a time.
-		out_valid			// whether the output is valid. Don't use data if 0.
+		decompressed_byte, // the decompressed output
+		out_valid,			// whether the output is valid. Don't use data if 0.
+		decompressor_busy	// whether the decompressor is busy. When this is 1'b1, the decompressor will ignore all data inputs.
 	);
-
+	
 	input clock, reset;
 	input[15:0] data_in;
 	input control_word_in;
 	input in_data_valid;
 	output history_full;
-	output[7:0] out_byte;
+	output[7:0] decompressed_byte;
 	output out_valid;
 
-	
+	// decompression fsm
+	// states:
+	//	D_IDLE: no input, not busy
+	//	D_PASS_THROUGH: regular mode -- no decompression needed on current byte
+	//	D_DECOMPRESS: Currently decompressing something
 
 
 
---
+++ decompressor/history_buffer.sv
@@ -0,0 +1,6 @@
+module history_buffer(
+		clock,
+		reset
+	);
+
+endmodule
