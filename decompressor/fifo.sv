f9b54db3a4ef1991db668aa026333e28fc42206c
pbridd <pbridd@users.noreply.github.com>
2017-11-10 21:54:05 -0800

    Finished preliminary implementation of FIFO

--- decompressor/fifo.sv
+++ decompressor/fifo.sv
@@ -20,17 +20,73 @@ module fifo(
 
 	// internal signals
 	logic[ADDR_WIDTH-1 : 0] front_ptr, back_ptr;
-	logic[7 : 0] fifo_buffer[FIFO_SIZE-1 : 0];
+	logic[ADDR_WIDTH-1 : 0] front_ptr_next, back_ptr_next;
+	logic[7 : 0] fifo_buffer[FIFO_SIZE-1 : 0], fifo_buffer_next[FIFO_SIZE-1 : 0];
 
 	assign fifo_full_out = (front_ptr == (back_ptr + 1)) ? 1 : 0;
 	assign fifo_empty_out = (front_ptr == back_ptr) ? 1 : 0;
+	assign data_out = fifo_buffer[front_ptr];
 
+	// registers
 	always_ff @(posedge clk, posedge reset) begin
 		if(reset) begin
 			front_ptr <= '0;
 			back_ptr <= '0;
+			for(int i = 0; i < FIFO_SIZE-1; i++)
+				fifo_buffer[i] <= '0;
 		end
+		else begin
+			front_ptr <= front_ptr_next;
+			back_ptr <= back_ptr_next;
+			fifo_buffer <= fifo_buffer_next;
+		end
+
+		// check that we never underflow
+		assert((front_ptr != back_ptr + 1) & ((front_ptr != 0) & (back_ptr != FIFO_SIZE-1)))
+		else $error("Underflow detected in FIFO %m");
 	end
 
+	// next state logic
+	always_comb begin
+		//default logic
+		front_ptr_next = front_ptr;
+		back_ptr = back_ptr_next;
+		fifo_buffer_next = fifo_buffer;
+
+		//advance both pointers
+		if(rd_en_in & wr_en_in) begin
+			if(front_ptr == (FIFO_SIZE-1))
+				front_ptr_next = '0;
+			else
+				front_ptr_next = front_ptr + 1;
+			if(back_ptr == (FIFO_SIZE-1))
+				front_ptr_next = '0;
+			else
+				back_ptr_next = back_ptr + 1;
+			fifo_buffer_next[back_ptr] = data_in;
+		end
+		else if(rd_en_in) begin
+			// make sure we aren't empty -- don't allow advance
+			// if FIFO is empty
+			if(front_ptr != back_ptr) begin
+				if(front_ptr == (FIFO_SIZE-1))
+					front_ptr_next = '0;
+				else
+					front_ptr_next = front_ptr + 1;
+			end
+		end
+		else if(wr_en_in) begin
+			// make sure we aren't full -- don't allow advance
+			// if FIFO is full
+			if((front_ptr != (back_ptr+1)) && ((front_ptr != 0) && (back_ptr != FIFO_SIZE-1))) begin
+				if(back_ptr == (FIFO_SIZE-1))
+					back_ptr_next = '0;
+				else
+					back_ptr_next = back_ptr + 1;
+				fifo_buffer_next[back_ptr] = data_in;
+			end
+		end
+
+	end
 
 endmodule
