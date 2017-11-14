4aac04b9be4035c5be6ea4341c8e83f879cecf33
pbridd <parker.ridd@gmail.com>
2017-11-13 22:57:29 -0800

    finished preliminary implementation of decompressor fsm. Not build tested

--- decompressor/decompressor_top.sv
+++ decompressor/decompressor_top.sv
@@ -19,14 +19,28 @@ module decompressor_top(
 
 	parameter HISTORY_SIZE = 4096;
 	localparam HISTORY_ADDR_WIDTH = $clog2(HISTORY_SIZE);
+
+	typedef struct packed {
+		logic[3:0] length;
+		logic[11:0] offset;
+	} compressed_t;
+
+	typedef union packed {
+		logic[15:0] character;
+		compressed_t compressed_objects;
+
+	} data_in_t;
 	
 	input clock, reset;
-	input[15:0] data_in;
+	input data_in_t data_in;
 	input control_word_in;
 	input in_data_valid;
 	output history_full;
 	output[7:0] decompressed_byte;
 	output out_valid;
+	output decompressor_busy;
+
+	
 
 	typedef enum {
 		IDLE,
@@ -35,17 +49,32 @@ module decompressor_top(
 	} decomp_state_type;
 
 	decomp_state_type decomp_state, decomp_state_next;
-	logic[HISTORY_ADDR_WIDTH-1:0] history_out_addr, history_out_addr_next;
+	logic[HISTORY_ADDR_WIDTH-1:0] history_out_addr, history_out_addr_next, history_in_addr, history_in_addr_next;
+	logic[HISTORY_ADDR_WIDTH-1:0] history_max_addr, history_max_addr_next;
+	logic history_buffer_wr_en;
+	logic[15:0] history_buffer_result;
+	data_in_t data_in_fp, data_in_fp_next;
+	logic control_word_in_fp, control_word_in_fp_next;
+
+	assign data_in_int = data_in;
 
 	// flip flops
 	always_ff @ (posedge clock, posedge reset) begin
 		if(reset) begin
 			decomp_state <= IDLE;
 			history_out_addr <= '0;
+			history_in_addr <= '0;
+			history_max_addr <= '0;
+			data_in_fp <= '0;
+			control_word_in_fp <= '0;
 		end
 		else if(clock) begin
 			decomp_state <= decomp_state_next;
 			history_out_addr <= history_out_addr_next;
+			history_in_addr <= history_in_addr_next;
+			history_max_addr <= history_max_addr_next;
+			data_in_fp <= data_in_fp_next;
+			control_word_in_fp <= control_word_in_fp_next;
 		end
 	end
 
@@ -57,24 +86,56 @@ module decompressor_top(
 	always_comb begin : decompressor_fsm
 		decomp_state_next = decomp_state;
 		history_out_addr_next = history_out_addr;
+		data_in_fp_next = data_in_fp;
+		control_word_in_fp_next = control_word_in_fp;
+		history_in_addr_next = history_in_addr;
+		decompressed_byte = '0;
+		out_valid = 1'b0;
+
+		decompressor_busy = 1'b1;
 		case(decomp_state)
 			IDLE: begin
+				decompressor_busy = 1'b0;
 				if(in_data_valid) begin
+					// 1. Flop the data
+					data_in_fp_next = data_in;
+					control_word_in_fp_next = control_word_in;
+
+					// 2. write data to the history buffer and increment history pointer
+					history_buffer_wr_en = 1'b1;
+					history_in_addr_next = history_in_addr + 1;
+
+					// 3. determine the next state
 					if(control_word_in == 1'b0) 
 						decom_state_next = PASS_THROUGH;
-					else
+					else begin
 						decomp_state_next = DECOMPRESS;
+						history_out_addr_next = history_in_addr - data_in.compressed_objects.offset;
+						history_max_addr = history_in_addr - data_in.compressed_objects.offset + length - 1;
+					end
+				end
 			end
 			PASS_THROUGH: begin
-				if(in_data_valid) begin
-					if(control_word_in == 1'b0) 
-						decom_state_next = PASS_THROUGH;
-					else
-						decomp_state_next = DECOMPRESS;
+				
+				//assign outputs
+				decompressed_byte = data_in_fp.character[7:0];
+				out_valid = 1'b1;
+
+				decomp_state_next = IDLE;
 			end
 			DECOMPRESS: begin
+				//assign outputs
+				decompressed_byte = history_buffer_result[7:0];
+				out_valid = 1'b1;
+
+				if(history_out_addr <= history_max_addr) begin
+					history_out_addr_next = history_out_addr + 1;
+				end
+				else
+					decom_state_next = IDLE;
 
 			end
+		endcase
 	end
 
 
