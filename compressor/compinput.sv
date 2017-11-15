82b3652dedfb18b91139ffcb9c82b479dc3b69fa
markcherni <32988160+markcherni@users.noreply.github.com>
2017-11-15 02:56:25 -0800

    input/history module
    tested and verified it works

--- compressor/compinput.sv
+++ compressor/compinput.sv
@@ -1,35 +1,58 @@
-module compinput(input clock, reset, valid, input [7:0] CurByte, input [11:0] offset, output logic [8*16:0] toCompare, output logic [23:0] toHash, output [11:0] OldByteOffset);
+module compinput(
+input clock, reset, valid, 
+input [15:0] [7:0] CurByte, 
+input [11:0] offset , 
+output logic [15:0] [7:0] toCompare,
+output logic [15:0] [7:0] NextBytes,
+output logic [23:0] toHash);
+
 parameter HISTORY = 4096;
 
 logic [HISTORY-1:0] [7:0] myHistory = '0;
-int count = 0;
 int bytePointer = 0;
+int count = 0;
+integer s_offset;
 
+assign s_offset = (!reset) ? integer' (offset) : '0 ;
 
-always_ff @(posedge clock, negedge reset) begin
+always_ff @(posedge clock) begin
 	
-	if (!reset) begin
-		count<= 0;
+	if (reset) begin
 		bytePointer <= 0;
-
+		count <= 0;
+		myHistory <= '0;
+		toHash <= '0;
+		toCompare <= '0;
+		NextBytes <= '0;
 	end
 	else begin
-
-		if(valid && count < HISTORY) begin
-			myHistory[count] <= CurByte;
-			count <= count + 1;
+		
+		// increment byte pointer if not at end of string
+		// add current byte to history
+		if(valid && (bytePointer < HISTORY)) begin
+			myHistory[count+1 +: 17] <= CurByte ;
+			count <= count + 16;
 		end
 		else count <= count;
-	
-		if (count - bytePointer == 3) begin
-			toHash <= {myHistory[bytePointer],myHistory[bytePointer+1],myHistory[bytePointer+2]};
-			bytePointer <= bytePointer + 1;
+		// if first 3 bytes recieved, send to hash function
+		
+		bytePointer <= bytePointer + 1;
+		if ((myHistory[bytePointer] != 0) && (bytePointer >= 3)) begin
+			toHash <= {myHistory[bytePointer-2],myHistory[bytePointer-1],myHistory[bytePointer]};			
 		end
 		else toHash <= toHash;
-
-		if (bytePointer - offset > 16)
-			toCompare <= myHistory[offset*16-1 : offset];
-		else 	toCompare <= {'0,myHistory[offset : bytePointer]};
+		// send 16 bytes to comparator
+		if ((myHistory[bytePointer] != 0) && (bytePointer - s_offset) > 15 && s_offset > 0) begin
+			toCompare <= myHistory[(s_offset) +: 16];
+			NextBytes <= myHistory[bytePointer +: 16];
+		end
+		else if ((myHistory[bytePointer] != 0)  && (bytePointer - s_offset) <= 15 && s_offset > 0) begin
+			for(int i = 0; i < bytePointer - offset; i++) begin
+				toCompare[i] <= myHistory[offset+i];
+			end
+			NextBytes <= myHistory[bytePointer +: 16];
+		end
+		else toCompare <= '0;
 	end
 end
 endmodule
