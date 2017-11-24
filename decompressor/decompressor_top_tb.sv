80abc85aef571fc412c174fec16d54597952073a
pbridd <pbridd@users.noreply.github.com>
2017-11-23 20:57:12 -0700

    Fixed output capture, hopefully for good

--- decompressor/decompressor_top_tb.sv
+++ decompressor/decompressor_top_tb.sv
@@ -136,9 +136,10 @@ module decompressor_top_tb;
 		
 		for(int j = 0; j < MAX_FILE_SIZE; j++) begin
 			@(dut_out_valid); 			// wait until the output byte is valid
+			@(negedge clock);			// wait at least until the next clock for the next byte
 				test_output_byte_array[j] = dut_decompressed_byte;
 				last_address_captured = j;
-			@(negedge clock);			// wait at least until the next clock for the next byte
+			
 		end
 
 	end
