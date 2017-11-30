835e1d7284597bed1490949d431ab8d68d94103a
pbridd <parker.ridd@gmail.com>
2017-11-30 00:45:24 -0800

    tentatively finished combined_top. fixed syntax errors

--- combined_top.sv
+++ combined_top.sv
@@ -5,7 +5,7 @@ module combined_top(
 		CurByte,
 		decompressed_byte,
 		out_valid,
-		decompressor_busy
+		finished_cycle,
 	);
 
 	parameter STRINGSIZE = 4096;
@@ -25,7 +25,6 @@ module combined_top(
 	input clock, reset, valid;
 	input [15:0] [7:0] CurByte;
 	output [7:0] decompressed_byte;
-	output out_valid, decompressor_busy;
 
 	// intermediate signals
 	logic c_Done;
@@ -34,10 +33,11 @@ module combined_top(
 	logic[15:0] d_data_in;
 	logic d_control_word_in;
 	logic d_data_in_valid;
-	logic unsigned integer c_controlPtr;
-	logic unsigned integer internal_controlPtr, internal_controlPtr_next;	
-	logic unsigned integer internal_dataPtr, internal_dataPtr_next;	
+	integer unsigned c_controlPtr;
+	integer unsigned internal_controlPtr, internal_controlPtr_next;	
+	integer unsigned internal_dataPtr, internal_dataPtr_next;	
 	logic d_out_valid;
+	logic decompressor_busy;
 
 	// state storage
 	combined_state_type comb_state, comb_state_next;
@@ -63,23 +63,46 @@ module combined_top(
 	end
 
 	always_comb begin
-		case(comb_state):
-			comb_state_next = comb_state;
+		comb_state_next = comb_state;
+		internal_controlPtr_next = internal_controlPtr;
+		internal_dataPtr = internal_dataPtr_next;
+		d_data_in = '0;
+		d_control_word_in = '0;
+		d_data_in_valid = 1'b0;
+		finished_cycle = 1'b0;
+
+		case(comb_state)
+
 			COMPRESS:
 				if(valid == 1'b0 && c_Done)
 					comb_state_next = DECOMPRESS;
-			DECOMPRESS:
+			DECOMPRESS: begin
 				d_data_in_valid = 1'b1;
-				d_data_in = compArray[internal_dataPtr];
 				d_control_word_in = c_controlWord[internal_controlPtr];
+				if(!decompressor_busy) begin
+					internal_controlPtr_next = internal_controlPtr + 1;
+					//figure out whether to pull one or two bytes from the table
+					if(compArray[internal_controlPtr] == 1'b0) begin
+						d_data_in = {8'b00000000, compArray[internal_dataPtr]};
+						internal_dataPtr_next = internal_dataPtr + 1;
+					end
+					else begin
+						d_data_in = {compArray[internal_dataPtr], compArray[intenal_dataPtr+1]};
+						internal_dataPtr_next = internal_dataPtr + 2;
+					end
+				end
+				
 				if((c_controlPtr == internal_controlPtr) && d_out_valid)
 					comb_state_next = DONE;
+			end
 			DONE:
+				finished_cycle = 1'b1;
+		endcase
 	end
 
 
 
-
+	assign out_valid = d_out_valid;
 
 
 		// module instantiations
